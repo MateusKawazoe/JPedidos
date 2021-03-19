bd8b6087a0020626493e84f8f564bcc0b433bc22
Mateuskwz <Matkawazoe@gmail.com>
2021-03-19 02:28:58 -0300

    Deu boa os testes

--- src/test/java/com/github/jpedidos/controller/OrderTest.java
+++ src/test/java/com/github/jpedidos/controller/OrderTest.java
@@ -2,6 +2,7 @@ package com.github.jpedidos.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.github.jpedidos.model.CRUD;
 import com.github.jpedidos.model.Connection;
 import java.text.DecimalFormat;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ public class OrderTest {
     DecimalFormat df = new DecimalFormat("0.00");
     Product product = new Product();
     Order order = new Order();
+    CRUD crud = new CRUD();
     Connection connection;
     User user = new User();
 
@@ -52,5 +54,37 @@ public class OrderTest {
     } catch (Exception e) {
       System.out.println(e);
     }
+
+    result = order.deletar(1);
+    assertEquals("false", result);
+
+    result =
+      crud.inserirModificarDeletar(
+        "ALTER TABLE client_order AUTO_INCREMENT = 1"
+      );
+    assertEquals("false", result);
+
+    result = user.deletar("matkawazoe@gmail.com");
+    assertEquals("false", result);
+
+    result =
+      crud.inserirModificarDeletar("DELETE FROM login WHERE login_id > 0");
+    assertEquals("false", result);
+
+    result =
+      crud.inserirModificarDeletar("ALTER TABLE login AUTO_INCREMENT = 1");
+    assertEquals("false", result);
+
+    result =
+      crud.inserirModificarDeletar("ALTER TABLE user AUTO_INCREMENT = 1");
+    assertEquals("false", result);
+
+    result =
+      crud.inserirModificarDeletar("DELETE FROM product WHERE product_id > 0");
+    assertEquals("false", result);
+
+    result =
+      crud.inserirModificarDeletar("ALTER TABLE product AUTO_INCREMENT = 1");
+    assertEquals("false", result);
   }
 }
